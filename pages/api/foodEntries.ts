6fe0f9c1cc8e52a7480a64ee26c73a07a9e282ed
Sólon <solonnoi@gmail.com>
2024-04-02 21:19:33 +0000

    komið inn form til að setja inn foods

--- pages/api/foodEntries.ts
+++ pages/api/foodEntries.ts
@@ -5,37 +5,41 @@ const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+    const { date } = req.query;
 
-    const tomorrow = new Date(today);
-    tomorrow.setDate(tomorrow.getDate() + 1);
+    if (date) {
+      const dateObj = new Date(date as string);
+      dateObj.setHours(0, 0, 0, 0);
+      const nextDay = new Date(dateObj);
+      nextDay.setDate(dateObj.getDate() + 1);
 
-    const foodEntries = await prisma.foodEntry.findMany({
-      where: {
-        dateCreated: {
-          gte: today,
-          lt: tomorrow,
+      const foodEntries = await prisma.foodEntry.findMany({
+        where: {
+          dateCreated: {
+            gte: dateObj,
+            lt: nextDay,
+          },
         },
-      },
-    });
-    return res.json(foodEntries);
+      });
+      return res.json(foodEntries);
+    } else {
+      const datesWithEntries = await prisma.foodEntry.findMany({
+        select: {
+          dateCreated: true,
+        },
+      });
+      const uniqueDates = datesWithEntries.map(entry => entry.dateCreated.toISOString().split('T')[0]);
+      console.log(uniqueDates);
+      return res.json(Array.from(new Set(uniqueDates)));
+    }
   } else if (req.method === 'POST') {
-    // Create a new food entry
     const { foodName, calories, protein } = req.body;
     const result = await prisma.foodEntry.create({
-      data: {
-        foodName,
-        calories,
-        protein,
-        // dateCreated is automatically set to now()
-      },
+      data: { foodName, calories, protein },
     });
     return res.json(result);
   }
-  
 
-  // Method Not Allowed handling
   res.setHeader('Allow', ['GET', 'POST']);
   res.status(405).end(`Method ${req.method} Not Allowed`);
 }
